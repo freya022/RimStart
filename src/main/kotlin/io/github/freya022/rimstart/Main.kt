86c3f70f0de56c2dd041e7b8dae51653bf366e1f
freya02 <41875020+freya022@users.noreply.github.com>
2023-07-29 16:13:10 +0200

    Add base UI

--- src/main/kotlin/io/github/freya022/rimstart/Main.kt
+++ src/main/kotlin/io/github/freya022/rimstart/Main.kt
@@ -1,10 +1,21 @@
 package io.github.freya022.rimstart
 
+import atlantafx.base.theme.CupertinoDark
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
-import kotlin.io.path.Path
+import io.github.oshai.kotlinlogging.KotlinLogging
+import javafx.application.Application
+import javafx.geometry.Insets
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.OverrunStyle
+import javafx.scene.layout.GridPane
+import javafx.stage.Stage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlin.io.path.*
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class ModsConfig @JsonCreator constructor(
@@ -38,19 +49,58 @@ fun RWList.toModsConfig(): ModsConfig {
 }
 
 object Main {
+    private val logger = KotlinLogging.logger { }
+
+    private val xmlMapper = XmlMapper()
+
     private val modsConfigPath = Path(System.getenv("appdata"), "..", "LocalLow", "Ludeon Studios", "RimWorld by Ludeon Studios", "Config", "ModsConfig.xml")
-    private val rwListPath = Path(System.getenv("appdata"), "..", "LocalLow", "Ludeon Studios", "RimWorld by Ludeon Studios", "ModLists", "Mods Multi 1.4 2.rml")
+    private val rwListFolderPath = Path(System.getenv("appdata"), "..", "LocalLow", "Ludeon Studios", "RimWorld by Ludeon Studios", "ModLists")
 
     @JvmStatic
-    fun main(args: Array<String>) {
-        val xmlMapper = XmlMapper()
-        val rwList = xmlMapper.readValue(rwListPath.toFile(), RWList::class.java)
+    @OptIn(ExperimentalPathApi::class)
+    fun main(args: Array<String>): Unit = runBlocking(Dispatchers.Main) {
+        Application.setUserAgentStylesheet(CupertinoDark().userAgentStylesheet)
+
         val modsConfig = xmlMapper.readValue(modsConfigPath.toFile(), ModsConfig::class.java)
-        val builtModsConfig = rwList.toModsConfig()
 
-        println("modsConfig = $modsConfig")
-        println("rwList = $rwList")
+        val root = GridPane().apply {
+            hgap = 10.0
+            vgap = 10.0
 
-        println("modsConfig == builtModsConfig = ${modsConfig == builtModsConfig}")
+            padding = Insets(10.0, 10.0, 10.0, 10.0)
+
+            rwListFolderPath.walk()
+                .filter { it.extension == "rml" }
+                .sortedByDescending { it.getLastModifiedTime() }
+                .map { path ->
+                    val rwList = xmlMapper.readValue(path.toFile(), RWList::class.java)
+
+                    logger.trace { "modsConfig = $modsConfig" }
+                    logger.trace { "rwList = $rwList" }
+
+                    val isCurrentConfig = modsConfig == rwList.toModsConfig()
+                    logger.debug { "modsConfig == builtModsConfig = $isCurrentConfig" }
+
+                    Button(path.nameWithoutExtension).apply {
+                        prefWidth = 150.0
+                        textOverrun = OverrunStyle.LEADING_WORD_ELLIPSIS
+                        isDefaultButton = isCurrentConfig
+
+                        setOnAction {
+                            logger.info { "Switching mod list to '${path.nameWithoutExtension}'" }
+                        }
+                    }
+                }
+                .chunked(4)
+                .forEachIndexed { index, buttons ->
+                    addRow(index, *buttons.toTypedArray())
+                }
+        }
+
+        Stage().apply {
+            title = "RimStart"
+            isResizable = false
+            scene = Scene(root)
+        }.show()
     }
 }
