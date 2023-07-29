7af7c2e782955f7737aa46d21b60120eb1c2ac98
freya02 <41875020+freya022@users.noreply.github.com>
2023-07-29 17:34:11 +0200

    Use Application, add XML writing

--- src/main/kotlin/io/github/freya022/rimstart/Main.kt
+++ src/main/kotlin/io/github/freya022/rimstart/Main.kt
@@ -4,7 +4,11 @@ import atlantafx.base.theme.CupertinoDark
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator
 import io.github.oshai.kotlinlogging.KotlinLogging
 import javafx.application.Application
 import javafx.geometry.Insets
@@ -13,15 +17,24 @@ import javafx.scene.control.Button
 import javafx.scene.control.OverrunStyle
 import javafx.scene.layout.GridPane
 import javafx.stage.Stage
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
 import kotlin.io.path.*
+import kotlin.system.exitProcess
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+//Jackson is stupid
+data class KnownExpansions @JsonCreator constructor(
+    @JsonProperty("li") @JacksonXmlElementWrapper(useWrapping = false) val li: List<String>
+)
+
+//Jackson is stupid
+data class ActiveMods @JsonCreator constructor(
+    @JsonProperty("li") @JacksonXmlElementWrapper(useWrapping = false) val li: List<String>
+)
+
+@JacksonXmlRootElement(localName = "ModsConfigData")
 data class ModsConfig @JsonCreator constructor(
     @JsonProperty("version") val version: String,
-    @JsonProperty("activeMods") val activeMods: List<String>,
-    @JsonProperty("knownExpansions") val knownExpansions: List<String>
+    @JsonProperty("activeMods") val activeMods: ActiveMods,
+    @JsonProperty("knownExpansions") val knownExpansions: KnownExpansions
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -43,25 +56,39 @@ data class RWList @JsonCreator constructor(
 fun RWList.toModsConfig(): ModsConfig {
     return ModsConfig(
         version = meta.gameVersion,
-        activeMods = modList.ids,
-        knownExpansions = modList.ids.filter { it.startsWith("ludeon.rimworld.") }.toHashSet().toList()
+        activeMods = ActiveMods(modList.ids),
+        knownExpansions = modList.ids.filter { it.startsWith("ludeon.rimworld.") }
+            .toHashSet().toList()
+            .let(::KnownExpansions)
     )
 }
 
 object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Application.launch(App::class.java)
+    }
+}
+
+class App : Application() {
     private val logger = KotlinLogging.logger { }
 
-    private val xmlMapper = XmlMapper()
+    private val xmlMapper = XmlMapper().apply {
+        configure(ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true)
+        configure(SerializationFeature.INDENT_OUTPUT, true)
+    }
 
-    private val modsConfigPath = Path(System.getenv("appdata"), "..", "LocalLow", "Ludeon Studios", "RimWorld by Ludeon Studios", "Config", "ModsConfig.xml")
-    private val rwListFolderPath = Path(System.getenv("appdata"), "..", "LocalLow", "Ludeon Studios", "RimWorld by Ludeon Studios", "ModLists")
+    private val rwFolderPath =
+        Path(System.getenv("appdata"), "..", "LocalLow", "Ludeon Studios", "RimWorld by Ludeon Studios")
+    private val modsConfigPath = rwFolderPath.resolve("Config").resolve("ModsConfig.xml")
+    private val rwListFolderPath = rwFolderPath.resolve("ModLists")
 
-    @JvmStatic
     @OptIn(ExperimentalPathApi::class)
-    fun main(args: Array<String>): Unit = runBlocking(Dispatchers.Main) {
-        Application.setUserAgentStylesheet(CupertinoDark().userAgentStylesheet)
+    override fun start(primaryStage: Stage) {
+        setUserAgentStylesheet(CupertinoDark().userAgentStylesheet)
 
-        val modsConfig = xmlMapper.readValue(modsConfigPath.toFile(), ModsConfig::class.java)
+        val modsConfigFile = modsConfigPath.toFile()
+        val modsConfig = xmlMapper.readValue(modsConfigFile, ModsConfig::class.java)
 
         val root = GridPane().apply {
             hgap = 10.0
@@ -78,7 +105,8 @@ object Main {
                     logger.trace { "modsConfig = $modsConfig" }
                     logger.trace { "rwList = $rwList" }
 
-                    val isCurrentConfig = modsConfig == rwList.toModsConfig()
+                    val rwListAsModsConfig = rwList.toModsConfig()
+                    val isCurrentConfig = modsConfig == rwListAsModsConfig
                     logger.debug { "modsConfig == builtModsConfig = $isCurrentConfig" }
 
                     Button(path.nameWithoutExtension).apply {
@@ -88,6 +116,11 @@ object Main {
 
                         setOnAction {
                             logger.info { "Switching mod list to '${path.nameWithoutExtension}'" }
+
+                            modsConfigPath.moveTo(modsConfigPath.resolveSibling("ModsConfig.xml.bak"), overwrite = true)
+                            xmlMapper.writeValue(modsConfigFile, rwListAsModsConfig)
+                            hostServices.showDocument("steam://rungameid/294100")
+                            exitProcess(0)
                         }
                     }
                 }
@@ -97,7 +130,7 @@ object Main {
                 }
         }
 
-        Stage().apply {
+        primaryStage.apply {
             title = "RimStart"
             isResizable = false
             scene = Scene(root)
